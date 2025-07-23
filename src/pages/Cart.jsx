23a35abb3f86140f578a56d23c14f3d2e5035f16
risalsalu <risalsalu15@gmail.com>
2025-07-23 16:27:20 +0530

    finished fill test

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-
+// chnage test
 function Cart({ cart, removeFromCart, updateQuantity, user }) {
   const navigate = useNavigate();
 
